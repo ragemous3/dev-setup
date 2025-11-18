6eb312475e0220ec4e2d4d84e92414a56b5a6a7a
ragemous3 <rasmus.krister.moberg@gmail.com>
2025-11-18 12:44:24 +0100

    Updated setup-script

--- setup.sh
+++ setup.sh
@@ -16,23 +16,25 @@ ln -sf "$PWD/.bash_profile" "$HOME/.bash_profile"
 mkdir -p "$HOME/.config"
 ln -sfn "$PWD/nvim" "$HOME/.config/nvim"
 
-# System packages (consider running separately)
+echo "Updating pacman and installing packages"
 sudo pacman -Syu
 sudo pacman -S lua-language-server base-devel fzf clang ripgrep python make openssh less
 
 # Build telescope-fzf-native
 ( cd "$HOME/.local/share/nvim/lazy/telescope-fzf-native.nvim" && make )
 
-# Install Rust (run as normal user)
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+read -p "Do you want rust installed? (y/n): " answer
 
-# Generate SSH key if missing
+if [ -z $answer ]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+# Configure git 
 mkdir -p "$HOME/.ssh"
 if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
   ssh-keygen -t ed25519 -C "$2"
 fi
 
-# Configure Git
 git config --global user.name "$1"
 git config --global user.email "$2"
 git config --global core.editor nvim
