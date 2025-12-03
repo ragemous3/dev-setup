2be22b43420214d6eac6b19b95ce61cafb13d04c
ragemous3 <rasmus.krister.moberg@gmail.com>
2025-12-03 21:43:57 +0100

    added better floating windows etc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,41 @@
-require("config.lazy")
+require('config.lazy')
 
 vim.opt.shiftwidth = 2
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true
 vim.g.have_nerd_font = false
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
-
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+vim.keymap.set('n', '<space>x', ':.lua<CR>')
+vim.keymap.set('v', '<space>x', ':lua<CR>')
+vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
+vim.keymap.set('n', '<space>asq', ':%s/"/\'/g<CR>')
 local job_id = 0
-vim.keymap.set("n", "<space>to", function()
+vim.keymap.set('n', '<space>to', function()
   vim.cmd.vnew()
   vim.cmd.term()
-  vim.cmd.wincmd("J")
+  vim.cmd.wincmd('J')
   vim.api.nvim_win_set_height(0, 5)
 
   job_id = vim.bo.channel
 end)
 
-local current_command = ""
-vim.keymap.set("n", "<space>te", function()
-  current_command = vim.fn.input("Command: ")
+local current_command = ''
+vim.keymap.set('n', '<space>te', function()
+  current_command = vim.fn.input('Command: ')
 end)
 
-vim.keymap.set("n", "<space>tr", function()
-  if current_command == "" then
-    current_command = vim.fn.input("Command: ")
+vim.keymap.set('n', '<space>tr', function()
+  if current_command == '' then
+    current_command = vim.fn.input('Command: ')
   end
 
-  vim.fn.chansend(job_id, { current_command .. "\r\n" })
+  vim.fn.chansend(job_id, { current_command .. '\r\n' })
 end)
 -- get into normal mode when in terminal mode
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
-vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+vim.keymap.set('n', '-', '<cmd>Oil<CR>')
 
 -- In order to have yank and ctrl+v work in windows.
 -- cd /usr/local/bin
@@ -54,12 +54,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set("n", "<space>d", function()
+  vim.diagnostic.open_float(nil, { focusable = false, border = "rounded" })
+end, { desc = "Show diagnostics in floating window" })
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = "●", -- or "E"/"W" if you prefer
-    source = true, -- show source of the diagnostic (lua-language-server)
-  },
+  virtual_text = false,
   signs = true, -- keep signs in the gutter
+  float = { border = "rounded" },
   underline = true, -- underline the problematic code
   update_in_insert = false,
 })
--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -13,7 +13,34 @@ return {
     },
     config = function()
       require'config.lsps.eslint'
+      require'config.lsps.typescript-language-server'
       require'config.lsps.lua'
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+	  local c = vim.lsp.get_client_by_id(args.data.client_id)
+	  if not c then return end
+
+	  local opts = { buffer = args.buf, silent = true }
+
+	  -- Common mappings for all LSPs
+	  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+
+
+	  if vim.bo.filetype == "lua" then
+	    -- Format the current buffer on save
+	    vim.api.nvim_create_autocmd("BufWritePre", {
+	      pattern = "*.lua",
+	      command = "normal! gg=G",
+	    })
+	  end
+	end,
+      })
+
     end,
   }
 }
