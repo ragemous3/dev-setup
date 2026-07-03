ce31030be205e4c1960500be7ffa7bbad08f1086
Rasmus <ragemous3@LAPTOP-QU4QSJST.localdomain>
2026-07-03 23:00:50 +0200

    Disable LTEX on slow WSL setup

--- nvim/lua/config/lsps/ltex-ls-plus.lua
+++ nvim/lua/config/lsps/ltex-ls-plus.lua
@@ -1,26 +1,20 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ltex_plus
 return {
-  setup = function(capabilities)
-    vim.lsp.config["ltex_plus"] = {
-      cmd = { vim.fn.expand("~/.local/bin/ltex-ls-plus") },
-      filetypes = { "markdown", "markdown.mdx", "mdx", "gitcommit" },
-      capabilities = capabilities,
-      settings = {
-        ltex = {
-          language = "en-GB",
-          completionEnabled = false,
-          checkFrequency = "save",
-          sentenceCacheSize = 0,
-          java = {
-            maximumHeapSize = 4096,
-          },
-          ["ltex-ls"] = {
-            logLevel = "warning",
-          },
-          enabled = { "gitcommit", "markdown", "mdx" },
-        },
-      },
-    }
-    vim.lsp.enable("ltex_plus");
-  end,
+	setup = function(capabilities)
+		vim.lsp.config("ltex_plus", {
+			cmd = { "env", "JAVA_OPTS=-XX:TieredStopAtLevel=1 -Xmx4096m", "/opt/ltex-ls-plus/bin/ltex-ls-plus" },
+			capabilities = capabilities,
+			settings = {
+				ltex = {
+					language = "en-GB",
+					checkFrequency = "save",
+					completionEnabled = false,
+					java = {
+						maximumHeapSize = 4096,
+					},
+				},
+			},
+		})
+		vim.lsp.enable("ltex_plus")
+	end,
 }
--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,48 +1,48 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      {
-	"folke/lazydev.nvim",
-	opts = {
-	  library = {
-	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{
+				"folke/lazydev.nvim",
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("config.lsps.eslint").setup(capabilities)
+			require("config.lsps.typescript-language-server").setup(capabilities)
+			require("config.lsps.lua").setup(capabilities)
+			require("config.lsps.marksman").setup(capabilities)
+			-- Disabled for now: LTEX eventually worked with JVM tuning, but startup and
+			-- first diagnostics still took several minutes on this WSL/Pentium setup.
+			require("config.lsps.ltex-ls-plus")
+			require("config.lsps.pyright").setup(capabilities)
+
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local c = vim.lsp.get_client_by_id(args.data.client_id)
+					if not c then
+						return
+					end
+
+					local opts = { buffer = args.buf, silent = true }
+					vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
+						buffer = args.buf,
+						silent = true,
+						desc = "LSP code actions",
+					})
+				end,
+			})
+		end,
 	},
-      },
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      require('config.lsps.eslint').setup(capabilities)
-      require('config.lsps.typescript-language-server').setup(capabilities)
-      require('config.lsps.lua').setup(capabilities)
-      require('config.lsps.marksman').setup(capabilities)
-      require('config.lsps.ltex-ls-plus').setup(capabilities)
-      require('config.lsps.pyright').setup(capabilities)
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(args)
-	  local c = vim.lsp.get_client_by_id(args.data.client_id)
-	  if not c then return end
-
-	  local opts = { buffer = args.buf, silent = true }
-	  vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions)
-	  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-	  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-	  -- Below ca is autocomplete based on suggested code actions
-	  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code actions" }, opts)
-
-	  if vim.bo.filetype == "lua" then
-	    -- Format the current buffer on save
-	    vim.api.nvim_create_autocmd("BufWritePre", {
-	      pattern = "*.lua",
-	      command = "normal! gg=G",
-	    })
-	  end
-	end,
-      })
-    end,
-  }
 }
